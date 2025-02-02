d03ccd4155b514bb93936b512268f359b7b7314b
aromot <aromot@gmail.com>
2025-02-02 21:44:06 +0100

    react-br ajouté

--- src/app/comments/columns.tsx
+++ src/app/comments/columns.tsx
@@ -2,6 +2,7 @@
 
 import { formatDateTime } from "@/lib/i18n";
 import { ColumnDef } from "@tanstack/react-table";
+import nl2br from "react-br";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -18,6 +19,7 @@ export const columns: ColumnDef<Directory>[] = [
   {
     accessorKey: "content",
     header: "Commentaire",
+    cell: ({ renderValue }) => nl2br(renderValue()),
   },
   {
     accessorKey: "createdAt",
