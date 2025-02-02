6c7569e122ea053c1b051ccf04e188d0cfada151
aromot <aromot@gmail.com>
2025-02-02 18:17:22 +0100

    chadcnui component added

--- src/app/comments/page.tsx
+++ src/app/comments/page.tsx
@@ -1,5 +1,16 @@
-const CommentPage = () => {
-  return <div>Ici la liste des commentaires</div>;
+import { Comment, PrismaClient } from "@prisma/client";
+
+const CommentPage = async () => {
+  const prisma = new PrismaClient();
+  const comments: Comment[] = await prisma.comment.findMany();
+
+  console.log({ comments });
+
+  if (comments.length === 0) {
+    return <div>Il n&apos;y a pas de commentaire</div>;
+  }
+
+  return <div>voici la liste</div>;
 };
 
 export default CommentPage;
