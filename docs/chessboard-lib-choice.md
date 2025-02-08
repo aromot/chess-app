9ecf112f45ef67c3cae1798b66753eb49d3193ab
aromot <aromot@gmail.com>
2025-02-08 18:47:24 +0100

    chessboard installed + premier test

---
+++ docs/chessboard-lib-choice.md
@@ -0,0 +1,40 @@
+# Choix de la library de gestion d'échiquier
+
+# Option 1 : react-chessboard
+
+https://github.com/Clariity/react-chessboard
+
+376 🌟 (08/02/2025)
+Cette lib semble bien mais il y a un bug à l'intégration. Ca ne marche pas.
+Un issue a été ouvert: https://github.com/Clariity/react-chessboard/issues/184
+Solution trouvée ici : https://github.com/vercel/next.js/discussions/49074
+Et l'auteur de la lib a répondu pour apporter sa réponse (et ça marche).
+
+# Option 2 : chessboardjsx
+
+272 🌟 (08/02/2025)
+
+https://github.com/willb335/chessboardjsx
+https://chessboardjsx.com/
+
+Cette lib n'est apparemment plus maintenue depuis 4 ans.
+
+# Option 3 : notre propre library
+
+Ca peut être très long...
+
+# Option 4 : React-Chessboard-Kit
+
+0 🌟 (08/02/2025)
+
+https://www.npmjs.com/package/react-chessboard-kit
+https://react-chessboard-kit.netlify.app/
+
+projet perso apparemment non maintenu, juste pour une petite démo.
+
+# Option 5 : Chessboard.js
+
+https://dev.to/kubeden/i-built-a-global-chess-board-from-scratch-completely-with-claude-nextjs-firebase-4cci
+https://github.com/kubeden/globchess-next-public
+
+Demo: https://www.globchess.com/
---
+++ src/app/chessboard/AppChessboard.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useState } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import cloneDeep from "lodash/cloneDeep";
+
+const AppChessboard = () => {
+  const [game, setGame] = useState<Chess>(new Chess());
+
+  console.log({ game });
+
+  // function makeRandomMove() {
+  //   console.log({ game });
+
+  //   const possibleMoves = game.moves();
+
+  //   console.log({ possibleMoves });
+
+  //   // exit if the game is over
+  //   if (game.isGameOver()) {
+  //     alert("Game over");
+  //     return;
+  //   }
+
+  //   if (game.isDraw()) {
+  //     alert("Draw");
+  //     return;
+  //   }
+
+  //   if (possibleMoves.length === 0) {
+  //     return;
+  //   }
+
+  //   const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+  //   safeGameMutate((game) => {
+  //     game.move(possibleMoves[randomIndex]);
+  //   });
+  // }
+
+  function onDrop(sourceSquare, targetSquare, piece) {
+    try {
+      const move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: piece[1].toLowerCase() ?? "q",
+      });
+      // console.log({ move });
+
+      // // illegal move => ça ne marche pas, on ne passe pas par là !
+      // if (move === null) {
+      //   return false;
+      // }
+    } catch (error) {
+      console.log(error.message);
+      console.log({ error });
+      return false;
+    }
+
+    setGame((game) => cloneDeep(game));
+
+    // store timeout so it can be cleared on undo/reset so computer doesn't execute move
+    // const newTimeout = setTimeout(makeRandomMove, 200);
+    // setCurrentTimeout(newTimeout);
+    return true;
+  }
+
+  return (
+    <div className="h-screen bg-zinc-900">
+      <div className="flex">
+        <div className="w-[600px] p-5">
+          <Chessboard
+            id="chessboard"
+            position={game.fen()}
+            onPieceDrop={onDrop}
+            customBoardStyle={{
+              borderRadius: "10px",
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)",
+            }}
+          />
+        </div>
+        <div className="flex-1 text-white py-5 px-3">
+          <div>Position :</div>
+          <div>{game.history().join(" ")}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppChessboard;
