68419feee901232fd26e9dfa839006536f24986c
mchriatt <mohammed.chriatt@gmail.com>
2025-02-10 00:22:19 +0100

    delete directrory function updated

--- src/directories/actions.ts
+++ src/directories/actions.ts
@@ -1,34 +1,39 @@
 "use server";
 
-import { z } from "zod";
-import { addDirectory } from "./db-queries";
+import { addDirectory, deleteDirectory } from "./db-queries";
 import { DirectorySchema } from "./schema";
-
 
 // Exporter les fonctions pour les utiliser dans les composants
 export async function createDirectory(name: string, white: boolean) {
   try {
     const result = DirectorySchema.safeParse({ name, white });
 
-    
     if (!result.success) {
       return {
-        error: result.error.errors[0].message
+        error: result.error.errors[0].message,
       };
     }
 
     await addDirectory({
       name,
-      white
+      white,
     });
 
     return { success: true };
-
   } catch (error) {
     console.log("Une erreur s'est produite");
     console.log({ error });
     return {
-      error: "Une erreur s'est produite lors de la création du répertoire"
+      error: "Une erreur s'est produite lors de la création du répertoire",
     };
+  }
+}
+
+export async function removeDirectory(id: number) {
+  try {
+    await deleteDirectory(id);
+  } catch (error) {
+    console.log("Une erreur s'est produite");
+    console.log({ error });
   }
 }
--- src/directories/components/delete/ButtonDeleteDirectory.tsx
+++ src/directories/components/delete/ButtonDeleteDirectory.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+"use client";
 
-const ButtonDeleteDirectory = props => {
-  return (
-    <div>ButtonDeleteDirectory</div>
-  )
+import { useState } from "react";
+import { Directory } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { removeDirectory } from "@/directories/actions";
+import { X } from "lucide-react";
+
+interface Props {
+  directory: Directory;
 }
 
-ButtonDeleteDirectory.propTypes = {}
+const ButtonDeleteDirectory: React.FC<Props> = ({ directory }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const router = useRouter();
 
-export default ButtonDeleteDirectory
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    await removeDirectory(directory.id);
+    setIsDeleting(false);
+    router.refresh();
+  };
+
+  return (
+    <button
+      onClick={handleDelete}
+      disabled={isDeleting}
+      className="h-8 w-8 p-0"
+    >
+      <X className="h-4 w-4" />
+
+      {isDeleting ? "Suppression en cours..." : ""}
+    </button>
+  );
+};
+
+export default ButtonDeleteDirectory;
