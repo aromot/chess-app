37102cb6e4db48c0dce0e0e1621ce548cccf5c30
aromot <aromot@gmail.com>
2025-03-15 12:54:22 +0000

    accueil membre mis à jour avec la liste des répertoires

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,38 +1,40 @@
-import { auth } from "@/lib/auth";
 import Title1 from "@/components/ui/title1";
-import { dbg } from "@/lib/helpers";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { URLS } from "../urls";
+import { checkAuth, dbg } from "@/lib/helpers";
+import { getDirectories } from "../directories/_db/db-queries";
+import ButtonAddDirectory from "../directories/_components/add/ButtonAddDirectory";
+import DirectoryProvider from "../directories/_components/DirectoryProvider";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "../directories/columns";
+import ModalEditDirectory from "../directories/_components/edit/ModalEditDirectory";
+import ModalDeleteDirectory from "../directories/_components/delete/ModalDeleteDirectory";
 
 const DashboardPage = async () => {
-  const session = await auth();
-
-  if (!session?.user) {
-    redirect("/");
-  }
-
-  dbg.info(session);
+  await checkAuth();
+  const directories = await getDirectories();
 
   return (
     <div>
-      <Title1>Bienvenue sur le Dashboard</Title1>
+      <Title1>Tableau de bord</Title1>
 
-      <div className="p-3">
-        <ul>
-          <li>
-            <Link href={URLS.directories}>Répertoires</Link>
-          </li>
-          <li>
-            <Link href="/comments">Commentaires</Link>
-          </li>
-          <li>
-            <Link href="/ui">Charte graphique</Link>
-          </li>
-          <li>
-            <Link href="/chessboard">Chessboard</Link>
-          </li>
-        </ul>
+      <div className="container mx-auto my-10 px-20 w-[80%]">
+        <div className="flex justify-between items-center mb-4">
+          <Title1>Vos répertoires</Title1>
+          {directories.length > 0 && <ButtonAddDirectory />}
+        </div>
+        <DirectoryProvider>
+          <DataTable
+            columns={columns}
+            data={directories}
+            noDataEntry={
+              <div className="py-5 space-y-5">
+                <div>Aucun répertoire pour le moment</div>
+                <ButtonAddDirectory />
+              </div>
+            }
+          />
+          <ModalEditDirectory />
+          <ModalDeleteDirectory />
+        </DirectoryProvider>
       </div>
     </div>
   );
